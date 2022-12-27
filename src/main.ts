1f553d175ffcf3e1e796052eca70cf0e505d4805
Some-Regular-Person <31082683+Some-Regular-Person@users.noreply.github.com>
2022-12-26 21:09:48 -0800

    Improve theming

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,68 @@ import { MarkdownPostProcessorContext, Plugin } from 'obsidian';
 import * as vegaLite from 'vega-lite';
 import * as vega from 'vega';
 import VegaLoader from './vegaLoader';
+import { CompileOptions } from 'vega-lite/build/src/compile/compile';
+
+const cssTextNormal = 'var(--text-normal)';
+const cssBackgroundModifierBorder = 'var(--background-modifier-border)';
+
+const compileOptions: CompileOptions = {
+    config: {
+        background: undefined,
+        view: {
+            stroke: cssBackgroundModifierBorder
+        }
+    }
+};
 
 export default class ObsidianVegaPlugin extends Plugin {
     loaderInstance = new VegaLoader(this);
+
+    getParseConfig = (font: string): vega.Config => {
+        return {
+            background: null,
+            group: {
+                stroke: cssBackgroundModifierBorder
+            },
+            style: {
+                'guide-label': {
+                    fill: cssTextNormal
+                },
+                'guide-title': {
+                    fill: cssTextNormal
+                }
+            },
+            axis: {
+                labelFont: font,
+                titleFont: font,
+                domainColor: cssTextNormal,
+                gridColor: cssBackgroundModifierBorder,
+                tickColor: cssTextNormal
+            },
+            legend: {
+                labelFont: font,
+                titleFont: font
+            },
+            title: {
+                font: font,
+                subtitleFont: font,
+                color: cssTextNormal,
+                subtitleColor: cssTextNormal,
+            }
+        }
+    };
 
     vegaCodeBlockProcessor = (isLite: boolean) => { return async (source: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
         try {
             const sourceAsJson = JSON.parse(source);
             const spec = isLite ?
-                    vegaLite.compile(sourceAsJson, {
-                        config: {
-                            background: undefined
-                        }
-                    }).spec :
+                    vegaLite.compile(sourceAsJson, compileOptions).spec :
                     sourceAsJson;
+            
+            const font = getComputedStyle(document.body).getPropertyValue('--font-mermaid');
+            const parseConfig = this.getParseConfig(font);
+            const runtime = vega.parse(spec, parseConfig);
 
-            const runtime = vega.parse(spec, {
-                background: null
-            });
             const view = new vega.View(runtime, {
                 renderer: 'svg',
                 loader: this.loaderInstance,
